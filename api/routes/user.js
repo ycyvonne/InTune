d7cabe01f94cca6b49d59e7a45b2b0e29898e266
Robert Zalog <rzalog@gmail.com>
2018-11-07 21:18:49 -0800

    Copy Brandon's changes.

--- api/routes/user.js
+++ api/routes/user.js
@@ -7,5 +7,10 @@ const userController = require('../controllers/UserController');
 
 router.get('/', userController.index);
 router.get('/name', userController.getName);
+router.get('/create', userController.create);
+router.get('/create/:id', userController.createFromId);
+router.get('/:id', userController.getUser)
+router.get('/get/:id', userController.getFromId);
+router.get('/delete/:id', userController.deleteUser)
 
 module.exports = router;
