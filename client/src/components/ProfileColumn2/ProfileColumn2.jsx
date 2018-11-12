ea1aab3cbe53d127ff20c895c2c76d24fccda45e
Stella Winghan So <sososostella@Stellas-MacBook-Pro.local>
2018-11-12 13:31:31 -0800

    profile page col 2

--- client/src/components/ProfileColumn2/ProfileColumn2.jsx
+++ client/src/components/ProfileColumn2/ProfileColumn2.jsx
@@ -5,52 +5,59 @@ class ProfileColumn2 extends Component {
   render() {
     return (
 
-      // <div className="profile-column2-wrapper">Profile Column2 here</div>
-      <div className="profile-title">FULL NAME
-      <form id="text-field">
-          <input type="text" />
-        </form><p></p>
-        <div className="profile-title">USERNAME
-        <form id="text-field">
+      <div className="profile-column2-wrapper">
+
+
+        <div>
+          <div className="profile-title">FULL NAME </div>
+          <form id="text-field">
             <input type="text" />
-          </form><p></p>
-          <div className="profile-title">SPOTIFY ACCOUNT
-      <form id="text-field">
+          </form>
+        </div>
+        <div>
+          <div className="profile-title">USERNAME </div>
+          <form id="text-field">
+            <input type="text" />
+          </form>
+        </div>
+        <div>
+          <div className="profile-title">SPOTIFY ACCOUNT </div>
+          <form id="text-field">
+            <input type="text" />
+          </form></div>
+        <div>
+          <div className="profile-title">FAVOURITE ARTIST </div>
+          <form id="text-field">
+            <input type="text" />
+          </form>
+        </div>
+
+        <div className="profile-col-2">
+
+          <div>
+            <div className="profile-title">EMAIL </div>
+            <form id="text-field">
               <input type="text" />
-            </form><p></p>
-            <div className="profile-title">FAVOURITE ARTIST
-      <form id="text-field">
-                <input type="text" />
-              </form><p></p>
-              <div className="profile-col-2">
-                <div className="profile-title">EMAIL
-                <form id="text-field">
-                    <input type="text" />
-                  </form><p></p>
-                </div>
-                <div className="profile-title">PASSWORD
-                <form id="text-field">
-                    <input type="text" />
-                  </form><p></p>
-                </div>
-                <div className="profile-title">LOCATION
-                <form id="text-field">
-                    <input type="text" />
-                  </form><p></p>
-                </div>
-              </div>
-            </div>
+            </form>
           </div>
+
+          <div>
+            <div className="profile-title">PASSWORD </div>
+            <form id="text-field">
+              <input type="text" />
+            </form>
+          </div>
+
+          <div>
+            <div className="profile-title">LOCATION </div>
+            <form id="text-field">
+              <input type="text" />
+            </form>
+          </div>
+
+
         </div>
       </div>
-
-
-
-      // <div className="profile-title"></div>
-
-
-
-
     );
 
   }
