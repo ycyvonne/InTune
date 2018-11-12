5c2538615ff8be35a8b37c772b108ee4b1c516e9
Stella Winghan So <sososostella@Stellas-MacBook-Pro.local>
2018-11-12 00:18:19 -0800

    profile page col 2

--- client/src/components/ProfileColumn2/ProfileColumn2.jsx
+++ client/src/components/ProfileColumn2/ProfileColumn2.jsx
@@ -3,7 +3,56 @@ import "./ProfileColumn2.scss";
 
 class ProfileColumn2 extends Component {
   render() {
-    return <div className="profile-column2-wrapper">Profile Column2 here</div>;
+    return (
+
+      // <div className="profile-column2-wrapper">Profile Column2 here</div>
+      <div className="profile-title">FULL NAME
+      <form id="text-field">
+          <input type="text" />
+        </form><p></p>
+        <div className="profile-title">USERNAME
+        <form id="text-field">
+            <input type="text" />
+          </form><p></p>
+          <div className="profile-title">SPOTIFY ACCOUNT
+      <form id="text-field">
+              <input type="text" />
+            </form><p></p>
+            <div className="profile-title">FAVOURITE ARTIST
+      <form id="text-field">
+                <input type="text" />
+              </form><p></p>
+              <div className="profile-col-2">
+                <div className="profile-title">EMAIL
+                <form id="text-field">
+                    <input type="text" />
+                  </form><p></p>
+                </div>
+                <div className="profile-title">PASSWORD
+                <form id="text-field">
+                    <input type="text" />
+                  </form><p></p>
+                </div>
+                <div className="profile-title">LOCATION
+                <form id="text-field">
+                    <input type="text" />
+                  </form><p></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+      // <div className="profile-title"></div>
+
+
+
+
+    );
+
   }
 }
 
