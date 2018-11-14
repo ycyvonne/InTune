b864474debe65a993abf86fa2cadfbc82c24fe23
Caroline-Quigg <carolinequigg@yahoo.com>
2018-11-14 11:44:23 -0800

    basic react tests for profile page

--- client/src/client_test/components/Button.test.js
+++ client/src/client_test/components/Button.test.js
@@ -3,15 +3,27 @@ import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
 import Button from '../../components/Button/Button';
 
+Enzyme.configure({ adapter: new Adapter() });
 
-function test() {
+function setup() {
+    const props = {
+        text: "test",
+        customClass: "testName"
+    }
 
+    const enzymeWrapper = shallow(<Button  text={props.text} customClass={props.customClass} />)
+
+    return {
+        props,
+        enzymeWrapper
+      }
 }
 
 
-describe('action creator', () =>(
-    it('dummy test',() => {
-        var test = "test";
-        expect(test).toBeDefined;
+describe('Button', () =>(
+    it('should render self',() => {
+        const {enzymeWrapper, props} = setup();
+        expect(enzymeWrapper.find('button').hasClass(props.customClass)).toBe(true);
+        expect(enzymeWrapper.find('button').text()).toBe(props.text);
     })
 ))
