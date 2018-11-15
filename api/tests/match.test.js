d69f46df5b20601df03e0c8ca988f1957fa9a645
Caroline-Quigg <carolinequigg@yahoo.com>
2018-11-15 09:49:00 -0800

    add more tests & edit readme

--- api/tests/match.test.js
+++ api/tests/match.test.js
@@ -19,6 +19,8 @@ var profile3 = {
   songs: ['song1']
 }
 
+var profile4 = {}
+
 describe('Test getScore', () => {
     test('Should have a score of 0 if no similarities between profiles', () => {
       expect(utils.getScore(profile1, profile2)).toEqual(0);
@@ -28,5 +30,17 @@ describe('Test getScore', () => {
       var moreSimilar = utils.getScore(profile1, profile3);
       var lessSimilar = utils.getScore(profile2, profile3);
       expect(moreSimilar).toBeGreaterThan(lessSimilar);
+    })
+
+    test ('Should be able to handle empty music profile', () => {
+      var testEmpty = utils.getScore(profile1,profile4);
+      expect (testEmpty).toBeDefined();
+      expect (testEmpty).toBe(0);
+    })
+
+    test ('Should be able to handle null profile', () => {
+      var testNull = utils.getScore(profile1, null);
+      expect (testNull).toBeDefined();
+      expect (testNull).toBe(0);
     })
 })
