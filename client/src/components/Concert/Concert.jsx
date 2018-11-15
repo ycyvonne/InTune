990ee55570b7422e3ab8a8266937df9c6710ff75
tanzeelak <tnkhan8042@gmail.com>
2018-11-14 23:49:50 -0800

    concert component and mapping

--- client/src/components/Concert/Concert.jsx
+++ client/src/components/Concert/Concert.jsx
@@ -3,12 +3,25 @@ import "./Concert.scss";
 
 class Concert extends Component {
   constructor(props) {
+    console.log("concert component");
     super(props);
     this.props = props;
-    this.state = {};
+    console.log(props);
   }
   render() {
-    return <div className="concert-wrapper">Concert item</div>;
+    return (
+      <div className="concert-wrapper">
+        {/* {this.props.name} */}
+        <div className="image-wrapper">
+          <img src="/img/concert-default.png" />
+        </div>
+        <div className="details-wrapper">
+          <div className="location">{this.props.location}</div>
+          <div className="title">{this.props.name}</div>
+          <div className="description">{this.props.artist}</div>
+        </div>
+      </div>
+    );
   }
 }
 
--- client/src/components/ConcertsPage/ConcertsPage.jsx
+++ client/src/components/ConcertsPage/ConcertsPage.jsx
@@ -20,20 +20,36 @@ class ConcertsPage extends Component {
   }
   render() {
     var isValid = true;
+    var data;
     if (
       !this.props.concerts.concertsData ||
       !this.props.concerts.concertsData.fetched
     ) {
       isValid = false;
     } else {
-      console.log(this.props.concerts.concertsData[0].displayName);
+      console.log(this.props.concerts.concertsData);
+      // data = Array.from(this.props.concerts.concertsData);
+      data = Object.values(this.props.concerts.concertsData);
+      data.pop();
+      console.log(data);
+      // console.log(this.props.concerts.concertsData[0].displayName);
     }
     return (
       isValid && (
         <div className="concert-discovery-wrapper">
           <div className="header">Concert Discovery</div>
           <div className="concert-list-wrapper">
-            <Concert />
+            {/* <Concert {...this.props} /> */}
+            {data.map(concert => {
+              return (
+                <Concert
+                  key={concert.id}
+                  name={concert.displayName}
+                  location={concert.venue.displayName}
+                  artist={concert.performance[0].displayName}
+                />
+              );
+            })}
           </div>
         </div>
       )
