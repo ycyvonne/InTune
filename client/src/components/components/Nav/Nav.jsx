f1ed5e78b5fa06b029bb69eb6f094a9622b06e26
yvonneCh <yc.yvonnechen@yahoo.com>
2018-11-27 12:03:04 -0800

    refactored client sass

--- client/src/components/components/Nav/Nav.jsx
+++ client/src/components/components/Nav/Nav.jsx
@@ -1,43 +1,49 @@
 import React, { Component } from "react";
 import "./Nav.scss";
-import { faBars, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faSearch, faMusic } from "@fortawesome/free-solid-svg-icons";
 import { SpotifyLoginBtn, Icon } from "../../";
 import ProfilePicture from "../ProfilePicture/ProfilePicture";
 
 class Nav extends Component {
   render() {
-    var profileButton;
+    var loginDependentComponents;
     if (
       this.props.user &&
       this.props.user.spotifyData != null &&
       this.props.user.spotifyData.isNewUser != null
     ) {
       // logged in, show profile pic
-      var name = this.props.user.spotifyData.name.split(" ")[0];
-      profileButton = (
+      loginDependentComponents = (
         <span className="nav-profile-button-wrapper">
+          <Icon icon={faMusic} />
           <ProfilePicture
             imageUrl={this.props.user.spotifyData.img}
             customSize="nav-picture-size"
             isCircle={true}
           />
-          <div className="nav-greeting">{name}</div>
         </span>
       );
     } else {
       // not logged in
-      profileButton = <SpotifyLoginBtn />;
+      loginDependentComponents = <SpotifyLoginBtn />;
     }
 
     return (
       <div className="navbar-wrapper">
         <div className="navbar">
+          <div className="navbar-inner">
           <div className="navbar-left">
-            <Icon icon={faBars} />
-            <Icon icon={faSearch} />
+              <h3>intune</h3>
+            </div>
+            <div className="navbar-middle">
+              <div className="navbar-link">MATCHES</div>
+              <div className="navbar-link">CONCERTS</div>
+            </div>
+            <div className="navbar-right">
+              <Icon icon={faSearch} />
+              {loginDependentComponents}
+            </div>
           </div>
-          <div className="navbar-middle">INTUNE</div>
-          <div className="navbar-right">{profileButton}</div>
         </div>
         <div className="invisible-margin" />
       </div>
