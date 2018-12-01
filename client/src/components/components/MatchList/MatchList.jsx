9eb793c54127b68b88215ddd117cf1e34b484766
yvonneCh <yc.yvonnechen@yahoo.com>
2018-12-01 02:37:12 -0800

    implement continuous side scroll on hover

--- client/src/components/components/MatchList/MatchList.jsx
+++ client/src/components/components/MatchList/MatchList.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { Match } from "../../";
+import { Match, Icon } from "../../";
 import "./MatchList.scss";
+import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 class MatchList extends Component {
   constructor(props) {
@@ -10,10 +11,11 @@ class MatchList extends Component {
       marginLeft: 0,
       matchesWindowWidth: 0,
       matchListWidth: 0,
-      marginMax: 0
+      marginMax: 0,
+      isHoveringLeft: false,
+      isHoveringRight: false,
+      currentInterval: null
     };
-    this.hoverLeft = this.hoverLeft.bind(this);
-    this.hoverRight = this.hoverRight.bind(this);
   }
 
   componentDidMount() {
@@ -24,15 +26,60 @@ class MatchList extends Component {
     });
   }
 
-  hoverLeft() {
-    if (this.state.marginLeft != 0) {
-      this.setState({ marginLeft: this.state.marginLeft + 100 });
+  setMoveInterval = (moveAmount) => {
+    return setInterval(() => {
+      if (moveAmount > 0) {
+        // moving left
+        if (this.state.marginLeft <= 0) {
+          this.setState({marginLeft: this.state.marginLeft + moveAmount});
+        }
+      }
+      else {
+        // moving right
+        if (Math.abs(this.state.marginLeft) < this.state.marginMax) {
+          this.setState({marginLeft: this.state.marginLeft + moveAmount});
+        }
+      }
+    }, 1);
+  }
+
+  hoverLeft = (amt) => {
+    if (this.state.marginLeft <= 0) {
+      var interval = this.setMoveInterval(amt);
+      this.setState({
+        isHoveringLeft: true,
+        currentInterval: interval
+      });
     }
   }
 
-  hoverRight() {
+  stopHoverLeft = () => {
+    if (this.state.isHoveringLeft) {
+      clearInterval(this.state.currentInterval);
+      this.setState({
+        isHoveringLeft: false,
+        currentInterval: null,
+      });
+    }
+  }
+
+  hoverRight = (amt) => {
     if (Math.abs(this.state.marginLeft) < this.state.marginMax) {
-      this.setState({ marginLeft: this.state.marginLeft - 100 });
+      var interval = this.setMoveInterval(-1 * amt);
+      this.setState({
+        isHoveringRight: true,
+        currentInterval: interval
+      });
+    }
+  }
+
+  stopHoverRight = () => {
+    if (this.state.isHoveringRight) {
+      clearInterval(this.state.currentInterval);
+      this.setState({
+        isHoveringRight: false,
+        currentInterval: null,
+      });
     }
   }
 
@@ -45,8 +92,26 @@ class MatchList extends Component {
           this.matchesWindow = matchesWindow;
         }}
       >
-        <div className="left-bar" onMouseEnter={this.hoverLeft} />
-        <div className="right-bar" onMouseEnter={this.hoverRight} />
+        <div className="left-bar"
+          onMouseEnter={() => this.hoverLeft(4)}
+          onMouseLeave={this.stopHoverLeft}
+        >
+          <Icon icon={faArrowLeft} />
+        </div>
+        <div className="left-bar-fast"
+          onMouseEnter={() => this.hoverLeft(10)}
+          onMouseLeave={this.stopHoverLeft}
+        />
+        <div className="right-bar"
+          onMouseEnter={() => this.hoverRight(4)}
+          onMouseLeave={this.stopHoverRight}
+        >
+          <Icon icon={faArrowRight} />
+        </div>
+        <div className="right-bar-fast"
+          onMouseEnter={() => this.hoverRight(10)}
+          onMouseLeave={this.stopHoverRight}
+        />
         <div
           style={{
             marginLeft: this.state.marginLeft + "pt"
@@ -56,10 +121,10 @@ class MatchList extends Component {
           }}
           className="match-list-wrapper"
         >
-          {matches.map(match => {
+          {matches.concat(matches).map((match, i) => {
             // TODO: add key prop when these are real matches
             return (
-              <div className="tile">
+              <div className="tile" key={i}>
                 <Match name={match.name} img={match.img} />
               </div>
             );
